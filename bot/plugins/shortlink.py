a3c9190e2e56c1838e486bb9e8e8803cb782740a
Finding.. <65222469+NikhilNGY@users.noreply.github.com>
2025-09-24 18:49:14 +0530

    Update shortlink.py

--- bot/plugins/shortlink.py
+++ bot/plugins/shortlink.py
@@ -1,1 +1,11 @@
-# Shortlink plugin
+import aiohttp
+from config import SHORTLINK_API
+
+async def shorten(url):
+    if not SHORTLINK_API:
+        return url
+    api_url = f"https://api.shortlinkservice.com/create?api={SHORTLINK_API}&url={url}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url) as r:
+            data = await r.json()
+            return data.get("short_url", url)# Shortlink plugin
